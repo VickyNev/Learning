1392a598ad1b6c5cfdc082176c0f986b7073c8d9
Viktoriia Nevidoma <>
2021-05-17 09:58:49 +0000

    Created class Book

--- main.dart
+++ main.dart
@@ -1,3 +1,31 @@
+import "dart:math";
+import "dart:io";
+
+class Book {
+  String title;
+  String author;
+  int numPages;
+}
+
+  /*Book(){
+    print ("love to read");
+  }*/
+
+
 void main() {
-	print('Hello World!');
+	
+  Book hp = Book();
+  hp.title = "Harry Potter: Sorcerers Stone";
+  hp.author = "JK Rowling";
+  hp.numPages = 300;
+
+  print (hp.title);
+
+  Book lotr = Book();
+  lotr.title = "Lord of the Rings";
+  lotr.author = "Tolkien";
+  lotr.numPages = 500;
+
+  print (lotr.author);
+
 }
